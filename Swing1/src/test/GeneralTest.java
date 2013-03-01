caf7e3ef3f7ea91c8f9f1066431e4e02d7a4fc6a
perand <dev@perand.com>
2013-03-01 06:10:48 +0100

    go on!

---
+++ Swing1/src/test/GeneralTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Perry
+ *
+ */
+public class GeneralTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
